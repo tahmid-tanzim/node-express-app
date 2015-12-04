56669ae655daa81a7adb5fc301607df79e1a416b
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-12-04 19:27:36 +0600

    Finishing our speakers' routes and views

--- routes/index.js
+++ routes/index.js
@@ -5,41 +5,54 @@ var appdata = require('../data.json');
 /* GET home template. */
 router.get('/', function (req, res) {
     var myArtwork = [];
+    var myArtists = appdata.speakers;
+
     appdata.speakers.forEach(function (item) {
         myArtwork = myArtwork.concat(item.artwork);
     });
 
     res.render('index', {
         title: 'Home',
-        artwork: myArtwork
+        artwork: myArtwork,
+        artists: myArtists,
+        page: 'home'
     });
 });
 
 /* GET speakers template. */
 router.get('/speakers', function (req, res) {
     var myArtwork = [];
+    var myArtists = appdata.speakers;
+
     appdata.speakers.forEach(function (item) {
         myArtwork = myArtwork.concat(item.artwork);
     });
 
     res.render('speakers', {
         title: 'Speakers',
-        artwork: myArtwork
+        artwork: myArtwork,
+        artists: myArtists,
+        page: 'artistList'
     });
 });
 
-/* GET speakers/:id template. */
+/* GET speakers details template. */
 router.get('/speakers/:speakerid', function (req, res) {
     var myArtwork = [];
+    var myArtists = [];
+
     appdata.speakers.forEach(function (item) {
         if(item.shortname == req.params.speakerid) {
+            myArtists.push(item);
             myArtwork = myArtwork.concat(item.artwork);
         }
     });
 
     res.render('speakers', {
         title: 'Speakers',
-        artwork: myArtwork
+        artwork: myArtwork,
+        artists: myArtists,
+        page: 'artistDetail'
     });
 });
 
