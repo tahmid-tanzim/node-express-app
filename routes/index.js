7a7d3edd100291824585f0f81a3283e663e30e1e
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-12-04 16:58:44 +0600

    Passing custom data through routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
 
 /* GET home template. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    var myArtwork = [];
+    appdata.speakers.forEach(function (item) {
+        myArtwork = myArtwork.concat(item.artwork);
+    });
+
+    res.render('index', {
+        title: 'Home',
+        artwork: myArtwork
+    });
 });
 
 module.exports = router;
